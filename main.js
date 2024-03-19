2d0d7af3da51945a750057a7859c96a9cb927197
sandransanchez <sandraosm07@gmail.com>
2024-03-18 22:31:36 -0600

    primera

---
+++ main.js
@@ -0,0 +1,10 @@
+function mostrar_ocultar(id){
+
+    let div = document.getElementById(id)
+
+    if (div.style.display == "none") {
+        div.style.display = "flex"
+    } else {
+        div.style.display = "none"
+    }
+}
